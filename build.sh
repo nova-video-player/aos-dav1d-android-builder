aa50e65bcaa6e88a1f39d75697030bf127eea960
Courville Software <software@courville.org>
2026-02-20 20:58:50 +0100

    dav1d 1.5.3

--- build.sh
+++ build.sh
@@ -107,7 +107,7 @@ else
 fi
 
 if [ ! -d dav1d ]; then
-  git -c http.sslVerify=false clone https://code.videolan.org/videolan/dav1d -b 1.5.1
+  git -c http.sslVerify=false clone https://code.videolan.org/videolan/dav1d -b 1.5.3
 fi
 
 cd dav1d
